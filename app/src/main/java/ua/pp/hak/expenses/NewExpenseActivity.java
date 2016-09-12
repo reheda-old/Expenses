0413417c7cbfaf63617ccd6ecaad2ed7caf8fc63
Valerii R <valerii.reheda@gmail.com>
2016-09-12 22:41:10 +0300

    added new activity and layout. changed MainActivity and activity_main

--- app/src/main/java/ua/pp/hak/expenses/NewExpenseActivity.java
+++ app/src/main/java/ua/pp/hak/expenses/NewExpenseActivity.java
@@ -4,19 +4,17 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class NewExpenseActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_new_expense);
     }
 
-
-    public void showNewExpenseActivity (View view){
-        Intent intent = new Intent(this, NewExpenseActivity.class);
+    public void showMainActivity(View view){
+        Intent  intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
 }
